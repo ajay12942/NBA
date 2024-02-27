dd6124ba90e443e31175d90bf9d61071d639f253
HARSHITH S GOWDA <160121266+Harshith5703@users.noreply.github.com>
2024-02-27 21:32:11 +0530

    Update into_position.php

--- insert_player/php/php_submit/into_position.php
+++ insert_player/php/php_submit/into_position.php
@@ -39,7 +39,7 @@ $sppf = $_POST["ppf"];
      <li><a href="../../../database/database.php">Database</a></li>
       <li><a href="../../../report/project_report.html">Report</a></li>
           <li><a href="../../../procedures/procedures.html">Procedures</a></li>
-          <li><a href="../../../about/about.html">About</a></li>
+          
 </ul>
 
 
